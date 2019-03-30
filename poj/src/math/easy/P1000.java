691d51097a1a545e6d6b6dd07f1b420531217c82
itso310 <zhyjky@163.com>
2019-03-30 16:28:39 +0800

    add comment

--- poj/src/math/easy/P1000.java
+++ poj/src/math/easy/P1000.java
@@ -1,3 +1,6 @@
+/**
+ * 计算两个整数的和
+ */
 package math.easy;
 
 import java.util.Scanner;
@@ -15,12 +18,25 @@ public class P1000 {
 		}
 	}
 
+	
+	/**
+	 * 判断输入的整数是否在规定的范围内
+	 * @param a 要相加的整数
+	 * @return 是否是有效数字
+	 */
 	private static boolean isValid(int a) {
 		if(a>=0 && a<=10)
 			return true;
 		return false;
 	}
 
+	
+	/**
+	 * 求两个整数的和
+	 * @param a 整数参数a
+	 * @param b 整数参数b
+	 * @return 两个参数的和
+	 */
 	private static int add(int a, int b) {
 		int sum = a + b;
 		return sum;
