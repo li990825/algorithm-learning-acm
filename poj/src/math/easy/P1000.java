5d575e5c5b85ecb2df669051b649d48accd8bb2e
itso310 <zhyjky@163.com>
2019-03-30 16:15:15 +0800

    A+B Problem

--- poj/src/math/easy/P1000.java
+++ poj/src/math/easy/P1000.java
@@ -1,7 +1,28 @@
 package math.easy;
 
+import java.util.Scanner;
+
 public class P1000 {
 	public static void main(String[] args) {
-		System.out.println("你好");
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		boolean fa = isValid(a);
+		boolean fb = isValid(b);
+		if(fa && fb) {
+			int sum = add(a, b);
+			System.out.println(sum);
+		}
+	}
+
+	private static boolean isValid(int a) {
+		if(a>=0 && a<=10)
+			return true;
+		return false;
+	}
+
+	private static int add(int a, int b) {
+		int sum = a + b;
+		return sum;
 	}
 }
